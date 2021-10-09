7620484c21cad818ddab2c97aee3e833bab98b87
Jayavignesh P <68550464+Jayavignesh-sys@users.noreply.github.com>
2021-10-10 00:22:49 +0530

    Update helper_user.go

--- helper_user/helper_user.go
+++ helper_user/helper_user.go
@@ -7,15 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// ConnectDB : This is helper function to connect mongoDB
-// If you want to export your function. You must to start upper case function name. Otherwise you won't see your function when you import that on other class.
 func ConnectDB() *mongo.Collection {
 	//config := GetConfiguration()
 	// Set client options
@@ -35,14 +30,11 @@ func ConnectDB() *mongo.Collection {
 	return collection
 }
 
-// ErrorResponse : This is error model.
 type ErrorResponse struct {
 	StatusCode   int    `json:"status"`
 	ErrorMessage string `json:"message"`
 }
 
-// GetError : This is helper function to prepare error model.
-// If you want to export your function. You must to start upper case function name. Otherwise you won't see your function when you import that on other class.
 func GetError(err error, w http.ResponseWriter) {
 
 	log.Fatal(err.Error())
@@ -57,24 +49,8 @@ func GetError(err error, w http.ResponseWriter) {
 	w.Write(message)
 }
 
-// Configuration model
 type Configuration struct {
 	Port             string
 	ConnectionString string
 }
 
-// GetConfiguration method basically populate configuration information from .env and return Configuration model
-func GetConfiguration() Configuration {
-	err := godotenv.Load("./.env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	configuration := Configuration{
-		os.Getenv("PORT"),
-		os.Getenv("CONNECTION_STRING"),
-	}
-
-	return configuration
-}
